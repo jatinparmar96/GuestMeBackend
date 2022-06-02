937d1a3c1b9d3f428bd49d97cd0b4e9e2e469336
Koichi <koichi89592@gmail.com>
2022-06-02 11:58:15 -0700

    Check database connection

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const MyModel = mongoose.model('Test', new Schema({ name: String }));
+
+
+app.get('/dbtest', async (req, res) => {
+  const model = new MyModel({});
+  model.name = new Date().toISOString();
+  model.save().then(result => {
+    res.send(result);
+  }).catch(
+    err => {
+      console.log(err);
+    }
+  );
+});
 
 app.get('/', (req, res) => {
   res.send('Hello GuesteaBackend')
@@ -8,3 +29,6 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT || 80, () => {
   console.log("Listening");
 });
+
+mongoose.connect(process.env.DATABASE_ACCESS, (result) => console.log("Database connected" + result));
+module.exports = mongoose.connection;
