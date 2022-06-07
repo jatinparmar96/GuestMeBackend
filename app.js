263772116ab1cba8e8cc32aa4a945f0859694d74
Koichi <koichi89592@gmail.com>
2022-06-06 23:03:16 -0700

    Set ESLint and workspace settings.json

--- app.js
+++ app.js
@@ -13,22 +13,25 @@ const MyModel = mongoose.model('Test', new Schema({ name: String }));
 app.get('/dbtest', async (req, res) => {
   const model = new MyModel({});
   model.name = new Date().toISOString();
-  model.save().then(result => {
-    res.send(result);
-  }).catch(
-    err => {
+  model
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
       console.log(err);
-    }
-  );
+    });
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello GuesteaBackend')
+  res.send('Hello GuesteaBackend');
 });
 
 app.listen(process.env.PORT || 80, () => {
-  console.log("Listening");
+  console.log('Listening');
 });
 
-mongoose.connect(process.env.DATABASE_ACCESS, (result) => console.log("Database connected" + result));
+mongoose.connect(process.env.DATABASE_ACCESS, (result) =>
+  console.log('Database connected' + result)
+);
 module.exports = mongoose.connection;
