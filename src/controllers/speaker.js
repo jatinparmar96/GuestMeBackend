360ebf25834363d0aa80057dcbdfde4cdc6bc976
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-06-11 18:06:42 -0700

    Add utils and login for speakers.

--- src/controllers/speaker.js
+++ src/controllers/speaker.js
@@ -1,27 +1,23 @@
 const bcrypt = require('bcrypt');
 const Speaker = require('../models/speaker');
 const jwt = require('jsonwebtoken');
+const { generateHash } = require('../utils/utils');
 /**
  * Handle registration of speaker and send token back.
+ * TODO:
+ * Validation, If Speaker already exists respond with status code (409 Conflict or 422 Unprocessable Entity)
  * @param {Speaker} req
  * @param {*} res
  * @param {*} next
  */
 exports.register = async (req, res, next) => {
   const speaker = new Speaker(req.body);
-
-  //10 Rounds is enough for our app
-  const salt = await bcrypt.genSalt(10);
-  //Generate Password HASH
-  speaker.password = await bcrypt.hash(
-    speaker.password,
-    salt
-  );
+  speaker.password = await generateHash(speaker.password);
   await speaker.save();
 
   //? By default the token expires in 30 days
   const token = jwt.sign(
-    { name: speaker.name },
+    { name: speaker.name, id: speaker._id },
     process.env.JWT_SECRET_ACCESS,
     { expiresIn: '30 days' }
   );
@@ -33,16 +29,41 @@ exports.register = async (req, res, next) => {
 
 /**
  * Sign In the Speaker
+ * TODO: Maybe Send user Object back without password included
  * @return
  *
  */
-exports.login = async (req, res, next) => {};
+exports.login = async (req, res, next) => {
+  const password = await generateHash(
+    req.body.password ?? ''
+  );
+  let user = await Speaker.findOne({
+    name: req.body.name,
+  });
+  const valid = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+  if (valid) {
+    const token = jwt.sign(
+      { name: user.name, id: user._id },
+      process.env.JWT_SECRET_ACCESS,
+      { expiresIn: '30 days' }
+    );
+    /**
+     * For now only send token, until we find a better way to mutate user object to remove
+     * password field
+     */
+    res.status(200).send({ token });
+  } else {
+    res.status(404).send();
+  }
+};
 
-// req will take parameters
+/**
+ * TODO: req will take parameters -> limit, offset, all
+ *  */
 exports.getSpeakers = async (req, res, next) => {
   const speakers = await Speaker.find({});
-  const speaker = new Speaker({ name: 'John Doe' });
-
-  speakers.push(speaker);
   res.json(speakers);
 };
