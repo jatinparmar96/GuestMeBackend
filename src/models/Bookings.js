958791fff8b7c9464cc1fab65e47cb3a6659ac5f
Arvind Kumar Suroju <74810455+ArvindKumarSuroju@users.noreply.github.com>
2022-06-19 11:45:23 -0700

    Add: Add Bookings schema

---
+++ src/models/Bookings.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+const bookingsSchema = new Schema({
+  speaker: mongoose.Schema({
+    id: {
+      type: Schema.Types.ObjectId, ref: 'Speaker',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  }),
+  organization: mongoose.Schema({
+    id: {
+      type: Schema.Types.ObjectId, ref: 'Organization',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  }),
+  bookingDateTime: mongoose.Schema({
+    startDateTime: {
+      type: Date,
+      required: true,
+    },
+    endDateTime: {
+      type: String,
+      required: true,
+    },
+  }),
+  location: ({
+    type: String
+  }),
+  topic: ({
+    type: String
+  }),
+  message: ({
+    type: String
+  }),
+  status: ({
+    type: String
+  }),
+  createdAt: ({
+    type: Date
+  }),
+  updatedAt: ({
+    type: Date
+  }),
+  personInCharge: ({
+    type: String
+  }),
+});
+
+module.exports = mongoose.model('Bookings', bookingsSchema);
