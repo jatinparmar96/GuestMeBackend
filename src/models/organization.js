0adb1388398f86c81d32b3e634091d7c1c0e2eed
Arvind Kumar Suroju <74810455+ArvindKumarSuroju@users.noreply.github.com>
2022-06-19 11:48:16 -0700

    Add: Add organizations DB Schema

---
+++ src/models/organization.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const organizationSchema = new Schema({
+  organizationName: {
+    type: String,
+  },
+  about: {
+    type: String,
+  },
+  profilePicture: {
+    type: String,
+  },
+  contact: mongoose.Schema({
+    phone: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+  }),
+  credentials: mongoose.Schema({
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  }),
+  savedSpeakers: mongoose.Schema({
+    speakerID: {
+       type: Schema.Types.ObjectId, ref: 'Speaker'
+    },
+    speakerName: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+    },
+    updatedAt: {
+      type: Date,
+    },
+  }),
+  notificationCount: ({
+    type: Number
+  })
+});
+
+module.exports = mongoose.model('Organization', organizationSchema);
